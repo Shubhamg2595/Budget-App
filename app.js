1b7b4de7ea5789af5582160f190ca8e408d1e22f
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-08 12:04:01 +0530

    DOMstrings

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var budgetController = (function() {})();
 
 var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn"
+  };
+
   return {
     getinput: function() {
       return {
-        type: document.querySelector(".add__type").value, //will be either inc(+) or exp(-)
-        description: document.querySelector(".add__description").value,
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc(+) or exp(-)
+        description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(".add__value").value
       };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
     // 1. get the filled input data
     var input = UICtrl.getinput();
@@ -25,7 +38,9 @@ var controller = (function(budgetCtrl, UICtrl) {
     console.log("aDDiTEM FUNC CALLED");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
