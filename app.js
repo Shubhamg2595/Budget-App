580d7b9cd17aa14ce59f85bd66fb9ab058115d12
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-08 11:37:18 +0530

    Event Listeners for Button Click and specific key presses

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
-})();
-/*
-in above lines of code, we have actually created a closure where variable x and method add are private ones .
-
-These private variables and methods cannot be  accessed by outside , however the public function publicTest can use these methods and  provide output as needed.
-*/
+var budgetController = (function() {})();
 
 var UIController = (function() {
   //some code
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    // console.log("Button was Clicked");
+    // 1. get the filled input data
+    // 2. add the item to budget controller
+    // 3. add the item to UI
+    // 5. calculate the budget
+    // 4. display the budget on UI
+  });
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      console.log("Enter Key was Pressed");
     }
-  };
+  });
 })(budgetController, UIController);
