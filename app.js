f36fbc4b90ccf67068461b52ccfdcad672997c26
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-08 11:54:13 +0530

    Read Input data from HTML

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var budgetController = (function() {})();
 
 var UIController = (function() {
-  //some code
+  return {
+    getinput: function() {
+      return {
+        type: document.querySelector(".add__type").value, //will be either inc(+) or exp(-)
+        description: document.querySelector(".add__description").value,
+        value: document.querySelector(".add__value").value
+      };
+    }
+  };
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
   var ctrlAddItem = function() {
     // 1. get the filled input data
+    var input = UICtrl.getinput();
+    console.log(input);
+
     // 2. add the item to budget controller
     // 3. add the item to UI
     // 5. calculate the budget
