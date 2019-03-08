268a77f5eb0a5e430a15b58da275f156f0e7bde6
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-08 11:45:35 +0530

    LAST COMMIT  UPDATED

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ var UIController = (function() {
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    // console.log("Button was Clicked");
+  var ctrlAddItem = function() {
     // 1. get the filled input data
     // 2. add the item to budget controller
     // 3. add the item to UI
     // 5. calculate the budget
     // 4. display the budget on UI
-  });
+    console.log("aDDiTEM FUNC CALLED");
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
-      console.log("Enter Key was Pressed");
+      // console.log("Enter Key was Pressed");
+      ctrlAddItem();
     }
   });
 })(budgetController, UIController);
